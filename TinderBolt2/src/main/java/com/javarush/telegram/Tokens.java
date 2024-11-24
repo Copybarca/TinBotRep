23c63be38bad80654b1c42adc7b04f912f6dd4ed
Lizunov Arthur <lizunov_artur@mail.ru>
2024-11-24 23:30:39 +0300

    update config and tokens

---
+++ TinderBolt2/src/main/java/com/javarush/telegram/Tokens.java
@@ -0,0 +1,36 @@
+package com.javarush.telegram;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Tokens {
+    public static final String TELEGRAM_BOT_NAME;
+    public static final String TELEGRAM_BOT_TOKEN;
+    public static final String OPEN_AI_TOKEN;
+    static{
+        Properties prop = new Properties();
+        String token = "src/main/resources/config/tgBot.config";
+        try (FileInputStream fis = new FileInputStream(token)) {
+            prop.load(fis);
+        } catch (FileNotFoundException ex) {
+            System.out.println("Tokens file missing");
+        } catch (IOException ex) {
+            System.out.println("Tokens missing");
+        }
+        TELEGRAM_BOT_NAME = prop.getProperty("tgBot.name");
+        TELEGRAM_BOT_TOKEN =prop.getProperty("tgBot.token");
+
+
+        token = "src/main/resources/config/gpt.config";
+        try (FileInputStream fis = new FileInputStream(token)) {
+            prop.load(fis);
+        } catch (FileNotFoundException ex) {
+            System.out.println("Tokens file missing");
+        } catch (IOException ex) {
+            System.out.println("Tokens missing");
+        }
+        OPEN_AI_TOKEN=prop.getProperty("gpt.token");
+    }
+}
